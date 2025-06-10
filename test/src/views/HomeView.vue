b4ed2932a0ef923c9819d8543b8545311a809467
Tuan Anh <tuananhnet1309@gmail.com>
2025-06-10 14:58:45 +0700

    u

---
+++ test/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <DiaChiComponent msg="Welcome to Your Vue.js App" />
+  </div>
+  <h1>Home Page Test</h1>
+</template>
+
+<script>
+import DiaChiComponent from "../components/DiaChiComponent.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    DiaChiComponent,
+  },
+};
+</script>
--- web-app/src/configurations/configuration.js
+++
@@ -1,5 +0,0 @@
-export const OAuthConfig = {
-  clientId: "782577513338-l11p43p5rr3k748tpjdu4dd96iij91gi.apps.googleusercontent.com",
-  redirectUri: "http://localhost:3000/authenticate",
-  authUri: "https://accounts.google.com/o/oauth2/auth",
-};
